8940a94281b197b6732a8e71fefda40c552385ef
João Gabriel <joaogabriel9633@gmail.com>
2024-05-30 20:04:18 -0300

    Userbox

--- messenger-clone/app/components/sidebar/MobileFooter.tsx
+++ messenger-clone/app/components/sidebar/MobileFooter.tsx
@@ -1,8 +1,30 @@
+'use client'
+
+import userConversation from '@/app/hooks/useConversation';
+import useRoutes from '@/app/hooks/useRoutes'
 import React from 'react'
+import MobileItem from './MobileItem';
 
 const MobileFooter = () => {
+  const routes = useRoutes();
+  const { isOpen } = userConversation();
+
+  if (isOpen) {
+    return null
+  }
+
   return (
-    <div>MobileFooter</div>
+    <div className='fixed justify-between w-full bottom-0 z-40 flex items-center bg-white border-t-[1px] lg:hidden'>
+      {routes.map((route) => (
+        <MobileItem
+          key={route.label}
+          href={route.href}
+          icon={route.icon}
+          active={route.active}
+          onClick={route.onClick}
+        />
+      ))}
+    </div>
   )
 }
 

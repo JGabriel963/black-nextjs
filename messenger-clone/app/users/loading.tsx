91897ea80e5fccbfe21312a6987865904c2891a4
João Gabriel <joaogabriel9633@gmail.com>
2025-01-25 14:44:22 -0300

    Começando de novo

--- messenger-clone/app/users/loading.tsx
+++
@@ -1,6 +0,0 @@
-import LoadingModal from "../components/LoadingModal"
-
-export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return <LoadingModal />
-  }

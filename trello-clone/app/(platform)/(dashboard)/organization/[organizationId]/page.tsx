c37020990e8659225f156e487faef8292c23f297
João Gabriel <joaogabriel9633@gmail.com>
2025-01-27 00:05:30 -0300

    Trello Clone: Depressão pós abstration

--- trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,12 +1,49 @@
-'use client'
-import { OrganizationSwitcher, useAuth } from "@clerk/nextjs"
+"use client"
+
+import { createBoard } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { useAction } from "@/hooks/use-action";
+
 
 
 export default function OrganizationIdPage() {
-    const { userId, orgId } = useAuth()
+    const { execute } = useAction(createBoard, {
+        onSucess: (data) => {
+            console.log(data, "Sucess")
+        },
+        onError: (error) => {
+            console.log(error)
+        }
+    });
+
+    const onSubmit = (formData: FormData) => {
+        const title = formData.get("title") as string;
+
+        execute({ title });
+    }
+
     return (
-        <div>
-            Organization ID: {orgId}
+        <div className="flex flex-col space-y-4">
+            <form action={onSubmit}>
+                <input 
+                    type="text" 
+                    name="title"
+                    required
+                    placeholder="Enter a board title"
+                    className="border-black border p-1"
+                />
+                <Button type="submit">
+                    Submit
+                </Button>
+            </form>
+
+            {/* <div className="space-y-2">
+                {boards.map((board) => (
+                    <div key={board.id}>
+                        Board title: {board.title}
+                    </div>
+                ))}
+            </div> */}
         </div>
     )
 }

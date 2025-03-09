3e514ca19ebb07071b7cbb7088e0879446dfd959
João Gabriel <joaogabriel9633@gmail.com>
2025-03-08 21:08:20 -0300

    Trello Clone: Card Form is done

--- trello-clone/app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ trello-clone/app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -1,5 +1,11 @@
+"use client"
+
 import { ListWithCards } from "@/types"
 import { ListHeader } from "./list-header";
+import { ComponentRef, useRef, useState } from "react";
+import { CardForm } from "./card-form";
+import { cn } from "@/lib/utils";
+import { CardItem } from "./card-item";
 
 
 interface ListItemProps {
@@ -11,10 +17,48 @@ export const ListItem = ({
     data,
     index
 }: ListItemProps) => {
+  const textareaRef = useRef<ComponentRef<"textarea">>(null)
+
+  const [isEditing, setIsEditing] = useState(false);
+
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  }
+
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textareaRef.current?.focus();
+    })
+  }
+
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
         <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
-            <ListHeader data={data} />
+            <ListHeader onAddCard={enableEditing} data={data} />
+            <ol
+              className={cn(
+                "mx-1 px-1 py-0.5 flex flex-col gap-y-2",
+                data.card.length > 0 ? "mt-2" : "mt-0"
+              )}
+            >
+              {data.card.map((card, index) => (
+                <CardItem
+                  index={index}
+                  key={card.id}
+                  data={card}
+                />
+              ))}
+            </ol>
+            <CardForm
+              listId={data.id}
+              ref={textareaRef}
+              isEditing={isEditing}
+              enableEditing={enableEditing}
+              disableEditing={disableEditing}
+            />
         </div>
     </li>
   )
